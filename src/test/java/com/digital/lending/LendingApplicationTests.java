a61801381bfc8f1c80a68dc48c29ba1b20b47c40
EinsteinNjoroge <NjorogeEinstein@gmail..com>
2026-06-05 13:05:22 +0300

    Initial project setup

---
+++ src/test/java/com/digital/lending/LendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
