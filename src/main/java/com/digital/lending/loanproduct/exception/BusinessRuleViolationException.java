1c3df9f386aecd2da0d5e71778dba609d2586212
Einstein Njoroge <ngugieinstein@gmail.com>
2026-06-09 14:35:42 +0300

    Revert "Loan products module"

--- src/main/java/com/digital/lending/loanproduct/exception/BusinessRuleViolationException.java
+++
@@ -1,5 +0,0 @@
-package com.digital.lending.loanproduct.exception;
-
-public class BusinessRuleViolationException extends RuntimeException {
-    public BusinessRuleViolationException(String message) { super(message); }
-}
--- src/main/java/com/digital/lending/loanproduct/repository/LoanProductAuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.digital.lending.loanproduct.repository;
-
-import com.digital.lending.loanproduct.model.LoanProductConfigurationAuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoanProductAuditLogRepository extends JpaRepository<LoanProductConfigurationAuditLog, Long> {
-}
