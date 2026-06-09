4816b36ea8b9bf4342516e9c088aeeb9cef3030d
EinsteinNjoroge <NjorogeEinstein@gmail..com>
2026-06-09 13:35:29 +0300

    Define service, controller ans error handling

---
+++ src/main/java/com/digital/lending/loanproduct/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.digital.lending.loanproduct;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
