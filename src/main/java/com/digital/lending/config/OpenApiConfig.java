0adb9daa42fc8de08e62f686848283b28cfd9037
EinsteinNjoroge <NjorogeEinstein@gmail..com>
2026-06-09 13:41:35 +0300

    Configure exception handling and swagger docs

--- src/main/java/com/digital/lending/config/OpenApiConfig.java
+++ src/main/java/com/digital/lending/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.digital.lending.config;
 
-import com.digital.lending.customer.util.CustomerApiConstants;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,13 +15,5 @@ public class OpenApiConfig {
                         .title("Core Lending Platform API Engine")
                         .version("v1.0.0")
                         .description("Production-grade modular lending core infrastructure engines."));
-    }
-
-    @Bean
-    public GroupedOpenApi customerModuleApi() {
-        return GroupedOpenApi.builder()
-                .group(CustomerApiConstants.GROUP_NAME)
-                .pathsToMatch(CustomerApiConstants.BASE_PATH + "/**")
-                .build();
     }
 }
